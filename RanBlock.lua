6283695e775d186b02ceac5e9cef877b1fcf5c52
puuror0 <125279870+Fransico0@users.noreply.github.com>
2023-04-14 21:45:14 +0800

    Update RanBlock.lua

--- RanBlock.lua
+++ RanBlock.lua
@@ -2,18 +2,20 @@
 
 local LP = game.Players.LocalPlayer
 local BP = LP.Backpack
+local CR = LP.Character
 local PG = LP.PlayerGui
 local WS = workspace
 local DB = true
 
 pcall(function()
-    if BP:FindFirstChild("Block") then
+    	if BP:FindFirstChild("Block") then
         local Tool = Instance.new("Tool")
         local Handle = BP.Block.Handle:Clone()
         local Outline = WS.GameStorage.Blocks.Block
         local Selection = PG.SelectionBox:Clone()
 
 	for _, plr in pairs(BP:GetChildren()) do if plr.Name == "RanBlock" then DB = false end end
+	for _, plr in pairs(CR:GetChildren()) do if plr.Name == "RanBlock" then DB = false end end
 	if DB then Tool.Parent = BP end
 	DB = true
 
@@ -25,8 +27,8 @@ pcall(function()
         Tool.Activated:Connect(function()
             local directions = {0, -8, 0}
 
-            local fire = {nil, Enum.NormalId.Top, LP.Character.HumanoidRootPart.Position + Vector3.new(directions[1],directions[2],directions[3])}
-            game:GetService("Players").LocalPlayer.Backpack.Block.RemoteEvent:FireServer(fire[1], fire[2], fire[3])
+            local fire = {nil, Enum.NormalId.Top, CR.HumanoidRootPart.Position + Vector3.new(directions[1],directions[2],directions[3])}
+            BP.Block.RemoteEvent:FireServer(fire[1], fire[2], fire[3])
         end)
         Tool.Equipped:Connect(function()
             Outline:Clone()
@@ -38,7 +40,7 @@ pcall(function()
             Selection.Adornee = Outline
             Selection.Parent = Outline
             while wait(0.001) do
-                Outline.Position = LP.Character:FindFirstChild("HumanoidRootPart").Position + Vector3.new(0, -8, 0)
+                Outline.Position = CR:FindFirstChild("HumanoidRootPart").Position + Vector3.new(0, -8, 0)
             end
         end)
         Tool.Unequipped:Connect(function()
